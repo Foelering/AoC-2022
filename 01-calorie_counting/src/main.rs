3e8e5e19e88a7a7b88fc77aa1a2594ba17170a1c
David Chiappini <foelering@gmail.com>
2023-02-04 11:01:39 +0100

    Day one done

--- 01-calorie_counting/src/main.rs
+++ 01-calorie_counting/src/main.rs
@@ -1,10 +1,28 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+
+#[derive(PartialEq, Eq, PartialOrd, Ord, Debug)]
 struct Elf {
     calories: i64,
 }
 
 fn main() {
+
+    println!("Best elf has {} calories", best_elf().calories);
+    let three = best_three();
+    println!("Best elves are {:?}", three);
+    let sum = {
+        let mut sum = 0;
+        for elf in three {
+            sum += elf.calories
+        }
+        sum
+    };
+    println!("Best elves summed {}", sum);
+}
+
+fn best_elf () -> Elf {
+    
     let file = File::open("./input").unwrap();
     let lines = BufReader::new(file).lines();
 
@@ -15,12 +33,11 @@ fn main() {
             Ok(y) => {
                 elf.calories += y;
             }
-            Err(err) => {
+            Err(_) => {
                 if elf.calories > best_elf.calories {
                     best_elf.calories = elf.calories;
                 }
                 elf.calories = 0;
-                println!("Done elf {:?}", err);
             }
         }
     }
@@ -29,6 +46,40 @@ fn main() {
         best_elf.calories = elf.calories;
         elf.calories = 0;
     }
+    
+    best_elf
+}
 
-    println!("Best elf has {} calories", best_elf.calories)
+fn best_three () -> Vec<Elf> {
+     
+    let file = File::open("./input").unwrap();
+    let lines = BufReader::new(file).lines();
+
+    let mut elf = Elf { calories: 0 };
+    let mut best_elf = vec!(
+        Elf { calories: 0 },
+        Elf { calories: 0 },
+        Elf { calories: 0 }
+    );
+    for line in lines {
+        match line.unwrap().parse::<i64>() {
+            Ok(y) => {
+                elf.calories += y;
+            }
+            Err(_) => {
+                if *best_elf.first().unwrap() < elf {
+                    best_elf[0].calories = elf.calories;
+                }
+                best_elf.sort();
+                elf.calories = 0;
+            }
+        }
+    }
+
+                if *best_elf.first().unwrap() < elf {
+                    best_elf[0].calories = elf.calories;
+                }
+                best_elf.sort();
+    
+    best_elf
 }
